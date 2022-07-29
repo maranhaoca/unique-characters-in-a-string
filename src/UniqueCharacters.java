abf040166d27ff833c8e6086b1452eefd69fe812
maranhaoca <maranhao.ca@gmail.com>
2022-07-29 14:37:56 +0100

    PR changes

--- src/UniqueCharacters.java
+++ src/UniqueCharacters.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 
 public class UniqueCharacters {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.println("Welcome to UniqueCharacters!!\nEnter a string!!");
+
+        Scanner input = new Scanner(System.in);
         String userInput = input.next();
-        if (uniqueCharacters(userInput)) {
-            System.out.println("It's " + uniqueCharacters(userInput) + ", this string has unique characters.");
+
+        boolean response = isUniqueCharacters(userInput);
+        if (response) {
+            System.out.println("It's " + response + ", this string has unique characters.");
         } else {
-            System.out.println("It's " + uniqueCharacters(userInput) + ", this string has no unique characters.");
+            System.out.println("It's " + response + ", this string has no unique characters.");
         }
     }
 
-    public static boolean uniqueCharacters(String userInput) {
+    public static boolean isUniqueCharacters(String userInput) {
         char[] array = userInput.toCharArray();
         boolean status = true;
+
         for (int i = 0; i < array.length; i++) {
             if(status) {
                 for (int k = i + 1; k < array.length; k++) {
